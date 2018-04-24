38ee83ba7354bea684c2172df4dd90428fab20e3
Giovanniko <jkoefman@gmail.com>
2018-04-24 20:33:58 +0000

    icon styles and listeners

--- js/app.js
+++ js/app.js
@@ -2,6 +2,74 @@
     var map;
     //Create array of markers
     var markers = [];
+
+    var styles = [
+          {
+            featureType: 'water',
+            stylers: [
+              { color: 'cyan' }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { color: '#ffffff' },
+              { weight: 6 }
+            ]
+          },{
+            featureType: 'administrative',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { color: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -40 }
+            ]
+          },{
+            featureType: 'transit.station',
+            stylers: [
+              { weight: 9 },
+              { hue: '#e85113' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'labels.icon',
+            stylers: [
+              { visibility: 'off' }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [
+              { lightness: 100 }
+            ]
+          },{
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [
+              { lightness: -100 }
+            ]
+          },{
+            featureType: 'poi',
+            elementType: 'geometry',
+            stylers: [
+              { visibility: 'on' },
+              { color: '#f0e4d3' }
+            ]
+          },{
+            featureType: 'road.highway',
+            elementType: 'geometry.fill',
+            stylers: [
+              { color: '#efe9e4' },
+              { lightness: -25 }
+            ]
+          }
+        ];
+
   
         // Constructor creates a new map - only center and zoom are required.
      function initMap() {
@@ -9,7 +77,8 @@
        map = new google.maps.Map(document.getElementById("map"), {
            center: {lat: 40.740386, lng: -73.920872},
            zoom: 18,
-           mapTypeControl: false
+           styles: styles,
+           mapTypeControl: false//user cannot change map type to satellite roads terrain etc
        });
 
        var locations = [
@@ -24,7 +93,12 @@
        ];
 
        var largeInfowindow = new google.maps.InfoWindow();
-//        var bounds = new google.maps.LatLngBounds();moved to showplaces function
+        //var bounds = new google.maps.LatLngBounds();moved to showplaces function
+
+        //Styles the markers, default marker
+        var defaultIcon = makeMarkerIcon('0091ff');
+
+        var highlightedIcon = makeMarkerIcon('FFFF24');
 
        //the loop takes the location array and ceates and array of markers on initilizing
        for (var i = 0; i <locations.length; i++) {
@@ -36,6 +110,7 @@
                //map: map,
                position: position,
                title: title,
+               icon: defaultIcon,
                animation: google.maps.Animation.DROP,
                id:i
            });
@@ -47,9 +122,18 @@
            marker.addListener('click', function() {
                populateInfoWindow(this, largeInfowindow);//this refers to the object to the left of the dot at function scall time
            });
+
+           //eventlisteners for hover over
+           marker.addListener('mouseover', function() {
+               this.setIcon(highlightedIcon);
+           });
+
+           marker.addListener('mouseout', function() {
+               this.setIcon(defaultIcon);
+           });
        }
 
-//        map.fitBounds(bounds);moved into showplaces function
+        //map.fitBounds(bounds);moved into showplaces function
 
        document.getElementById('show-places').addEventListener('click', showPlaces);
        document.getElementById('hide-places').addEventListener('click', hidePlaces);
@@ -84,6 +168,17 @@
            for(var i = 0; i < markers.length; i++) {
                markers[i].setMap(null);
            }
+       }
+
+       function makeMarkerIcon(markerColor) {
+           var markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+          '|40|_|%E2%80%A2',
+           new google.maps.Size(21, 34),
+           new google.maps.Point(0, 0),
+           new google.maps.Point(10, 34),
+           new google.maps.Size(21, 34));
+         return markerImage;
        }
 
 //        var woodside = {lat: 40.740386, lng: -73.920872
